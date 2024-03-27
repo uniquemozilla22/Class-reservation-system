935c4d777b2eec51ad5adbb651924faddd73ba41
uniquemozilla22 <uniqgaming26@gmail.com>
2024-03-26 21:23:33 -0400

    GCP Integration finalization

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
+app.set('trust proxy', true);
 app.get("/",(req,res)=>{
     res.send({message:"Application API is running "})
 })
-
 
 app.listen(8080, ()=> console.log("Application is running on http://localhost:8080"))
