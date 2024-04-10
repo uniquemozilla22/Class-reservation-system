5acc4c8f0cb92e0babe5ebe72c82933c661ee4fb
uniquemozilla22 <uniqgaming26@gmail.com>
2024-04-10 00:51:04 -0400

    Update on the Features

--- README.md
+++
@@ -1,8 +0,0 @@
-# Class Reservation System
-
-
-Working on the server
-
-## Phase 3
-
-Setting up the project.
--- src/utils/localStorage.js
+++
@@ -1,9 +0,0 @@
-
-
-
-export const getItem = (token) =>{
-    return localStorage.getItem(token)
-}
-export const setItem = (token, value) =>{
-    return localStorage.setItem(token ,value)
-}
